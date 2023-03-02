12e1b4de0255c6896f71b6927893f6d761b69906
kasun <kasunvimu8@gmail.com>
2023-03-02 13:36:45 +0530

    next first hand on experience

--- pages/news/[newsId].tsx
+++ pages/news/[newsId].tsx
@@ -1,5 +1,9 @@
+import { useRouter } from "next/router";
 
 const Detail = () => {
+  const router = useRouter();
+  const { newsId } = router.query;
+  console.log(router)
   return (
     <div>News Detail</div>
   )
